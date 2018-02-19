29cc7883d7841b0b783da395ba13fef7a52056ec
rnr56 <rnr56@txstate.edu>
2018-02-19 17:55:26 -0600

    Update hello_bob.ino

--- hello_bob.ino
+++ hello_bob.ino
@@ -5,6 +5,31 @@ int main
 {
  
 cout << "Hello, bob!\n";
-    
-    return 0;
+ 
+ 
+ int motorPin = 3;
+ 
+void setup() 
+{ 
+  pinMode(motorPin, OUTPUT);
+  Serial.begin(9600);
+  while (! Serial);
+  Serial.println("Speed 0 to 255");
+} 
+ 
+ 
+void loop() 
+{ 
+  if (Serial.available())
+  {
+    int speed = Serial.parseInt();
+    if (speed >= 0 && speed <= 255)
+    {
+      analogWrite(motorPin, speed);
+    }
+  }
+} 
+ 
+ 
+ return 0;
 }
